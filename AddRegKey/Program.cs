d349298ba0446ea0897bfa100abb983ceabef7f5
Dima <d.a.shimanov@gmail.com>
2017-10-02 09:31:24 +0700

    Upload project

---
+++ AddRegKey/Program.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Win32;
+
+namespace AddRegKey
+{
+    class Program
+    {
+        protected void Main()
+        {
+            Registry.SetValue("HKEY_CURRENT_USER\\Software\\Validata\\zcs", "TimeWarnExpiredCert", "00,00,00,00", RegistryValueKind.Binary);
+            Registry.SetValue("HKEY_CURRENT_USER\\Software\\Validata\\zcs", "TimeWarnExpiredCrl", "00,00,00,00", RegistryValueKind.Binary);
+        }
+    }
+}
