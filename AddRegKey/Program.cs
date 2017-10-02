c9aca4c9fa99a0346375dd4f39918fe40515e2b2
Dima <d.a.shimanov@gmail.com>
2017-10-02 09:36:34 +0700

    Fix bug

--- AddRegKey/Program.cs
+++ AddRegKey/Program.cs
@@ -4,7 +4,7 @@ namespace AddRegKey
 {
     class Program
     {
-        protected void Main()
+        protected static void Main()
         {
             Registry.SetValue("HKEY_CURRENT_USER\\Software\\Validata\\zcs", "TimeWarnExpiredCert", "00,00,00,00", RegistryValueKind.Binary);
             Registry.SetValue("HKEY_CURRENT_USER\\Software\\Validata\\zcs", "TimeWarnExpiredCrl", "00,00,00,00", RegistryValueKind.Binary);
