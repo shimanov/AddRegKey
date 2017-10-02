f4ebd2d7632dbb78258ba666d2389f2025e4b7b6
Dima <d.a.shimanov@gmail.com>
2017-10-02 10:25:37 +0700

    Добавил обработку исключения

--- AddRegKey/Program.cs
+++ AddRegKey/Program.cs
@@ -4,11 +4,20 @@ namespace AddRegKey
 {
     class Program
     {
-        protected static void Main()
+        protected static int Main()
         {
-            byte[] data = { 0x00, 0x00, 0x00, 0x00 };
-            Registry.SetValue("HKEY_CURRENT_USER\\Software\\Validata\\zcs", "TimeWarnExpiredCert", data, RegistryValueKind.Binary);
-            Registry.SetValue("HKEY_CURRENT_USER\\Software\\Validata\\zcs", "TimeWarnExpiredCrl", data, RegistryValueKind.Binary);
+            try
+            {
+                byte[] data = { 0x00, 0x00, 0x00, 0x00 };
+                Registry.SetValue("HKEY_CURRENT_USER\\Software\\Validata\\zcs", "TimeWarnExpiredCert", data, RegistryValueKind.Binary);
+                Registry.SetValue("HKEY_CURRENT_USER\\Software\\Validata\\zcs", "TimeWarnExpiredCrl", data, RegistryValueKind.Binary);
+
+                return 0;
+            }
+            catch (System.Exception)
+            {
+                return 1;
+            }
         }
     }
 }
