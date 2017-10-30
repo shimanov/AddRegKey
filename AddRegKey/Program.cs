b14d7eab5e9a3c1adc324b12d8b3facc545457b5
Dima <d.a.shimanov@gmail.com>
2017-10-30 11:33:31 +0700

    Добавлены настройки службы

--- AddRegKey/Program.cs
+++ AddRegKey/Program.cs
@@ -12,6 +12,32 @@ namespace AddRegKey
                 Registry.SetValue("HKEY_CURRENT_USER\\Software\\Validata\\zcs", "TimeWarnExpiredCert", data, RegistryValueKind.Binary);
                 Registry.SetValue("HKEY_CURRENT_USER\\Software\\Validata\\zcs", "TimeWarnExpiredCrl", data, RegistryValueKind.Binary);
 
+                //Type
+                string type = "16";
+                Registry.SetValue("HKEY_LOCAL_MACHINE\\SYSTEM\\CurrentControlSet\\services\\zCSSVC", "Type", type, RegistryValueKind.DWord);
+
+                //Start
+                string start = "2";
+                Registry.SetValue("HKEY_LOCAL_MACHINE\\SYSTEM\\CurrentControlSet\\services\\zCSSVC", "Start", start, RegistryValueKind.DWord);
+
+                //ErrorControl
+                string errorControl = "1";
+                Registry.SetValue("HKEY_LOCAL_MACHINE\\SYSTEM\\CurrentControlSet\\services\\zCSSVC", "ErrorControl", errorControl, RegistryValueKind.DWord);
+
+                //DelayedAutostart
+                string delayedAutostart = "1";
+                Registry.SetValue("HKEY_LOCAL_MACHINE\\SYSTEM\\CurrentControlSet\\services\\zCSSVC", "DelayedAutostart", delayedAutostart, RegistryValueKind.DWord);
+
+                //FailureActionsOnNonCrashFailures
+                string failureActionsOnNonCrashFailures = "1";
+                Registry.SetValue("HKEY_LOCAL_MACHINE\\SYSTEM\\CurrentControlSet\\services\\zCSSVC", "FailureActionsOnNonCrashFailures", failureActionsOnNonCrashFailures, RegistryValueKind.DWord);
+
+                //FailureActions
+                byte[] failureActions = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x03, 0x00, 0x00, 0x00, 0x14, 0x00, 0x00,
+                                          0x00, 0x01, 0x00, 0x00, 0x00, 0xe0, 0x93, 0x04, 0x00, 0x01, 0x00, 0x00, 0x00, 0xe0, 0x93, 0x04, 0x00, 0x01, 0x00,
+                                          0x00, 0x00, 0xe0, 0x93, 0x04, 0x00 };
+                Registry.SetValue("HKEY_LOCAL_MACHINE\\SYSTEM\\CurrentControlSet\\services\\zCSSVC", "FailureActions", failureActions, RegistryValueKind.Binary);
+
                 return 0;
             }
             catch (System.Exception)
--- README.md
+++ README.md
@@ -1,7 +1,6 @@
 # AddRegKey
 
 [![Codacy Badge](https://api.codacy.com/project/badge/Grade/7648861153df4b53bdf22029cb7b83ac)](https://www.codacy.com/app/shimanov/AddRegKey?utm_source=github.com&utm_medium=referral&utm_content=shimanov/AddRegKey&utm_campaign=badger)
-
 [![MIT License](http://b.repl.ca/v1/License-MIT-red.png)](LICENSE)
 
 ### Функционал приложения
